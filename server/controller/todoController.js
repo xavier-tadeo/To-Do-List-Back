73fcd26db613cef0aaeb85cc4ab0b9c360f9c8ad
Tadeo <domenechtadeo@outlook.es>
2021-12-17 10:51:44 +0100

    Delete the console.log for try function

--- server/controller/todoController.js
+++ server/controller/todoController.js
@@ -1,10 +1,8 @@
 const Todo = require("../../database/models/todo");
 
 const getTodo = async (req, res, next) => {
-  console.log("holaaaaaaaaaaaaaa");
   try {
     const allTodo = await Todo.find();
-    console.log(allTodo);
     res.json(allTodo);
   } catch (error) {
     next(error);
