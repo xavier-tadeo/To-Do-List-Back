bdd6a0ca99585981deecdda7d5edb7081547a4cb
Tadeo <domenechtadeo@outlook.es>
2021-12-20 23:08:29 +0100

    Create the delete task

--- server/routes/routesTodo.js
+++ server/routes/routesTodo.js
@@ -1,10 +1,15 @@
 const express = require("express");
 
-const { getTodo, createTodoList } = require("../controller/todoController");
+const {
+  getTodo,
+  createTodoList,
+  deleteTodoList,
+} = require("../controller/todoController");
 
 const router = express.Router();
 
 router.get("/", getTodo);
 router.post("/create", createTodoList);
+router.delete("/delete/:id", deleteTodoList);
 
 module.exports = router;
